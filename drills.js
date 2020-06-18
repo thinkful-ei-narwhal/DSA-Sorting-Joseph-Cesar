5d8307a0a05a3ffef4d43e53d23eeeea3422f072
antonibrivera <josephr98@gmail.com>
2020-06-18 14:13:24 -0400

    finished drill 2

--- drills.js
+++ drills.js
@@ -16,6 +16,20 @@
   4. [21,1,26,45] [29,28,2,9] are the two lists used on the 7th merge
 */
 
-/*
 
+// 2. Understanding quicksort
+/*
+  1. c) Neither 14 nor 17 could have been the pivot
+
+  2. [14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+
+    LAST ITEM IN PIVOT
+    [10, 17, 13, 15, 19, 14, 3, 16, 9, 12]
+    
+    FIRST ITEM IN PIVOT
+    Because the first item is pivot, we start at the end of the array and loop backwards.
+    [14, 17, 13, 15, 19, 10, 3, 12, 9, 16]
 */ 
+
+// 3. Implementing quicksort
+
