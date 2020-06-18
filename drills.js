16c715cbb592f642a961be05054d765a1ef9c2a6
Cesar Morales <cesarlenin2@live.com>
2020-06-18 11:50:38 -0700

    finished number 4

--- drills.js
+++ drills.js
@@ -1,4 +1,5 @@
 const quickSort = require('./quickSort');
+const mergeSort = require('./mergeSort');
 
 // 1. Understanding merge sort
 // given this array [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
@@ -37,8 +38,15 @@ const quickSort = require('./quickSort');
 
 function qSort(array) {
   return quickSort(array);
-};
+}
 
 const testArray = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
 
 console.log(qSort(testArray));
+
+// 4. Implementing merge sort
+
+function mSort(arr){
+  return mergeSort(arr);
+}
+console.log(mergeSort(testArray));
