4ab540d9f7405d26c679fcdb83fbce187eb8bb13
antonibrivera <josephr98@gmail.com>
2020-06-18 15:46:32 -0400

    completed drill 5

--- drills.js
+++ drills.js
@@ -42,22 +42,39 @@ function qSort(array) {
 
 const testArray = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
 
-console.log(qSort(testArray));
+// console.log(qSort(testArray));
 
 // 4. Implementing merge sort
 function mSort(arr){
   return mergeSort(arr);
 }
-console.log(mergeSort(testArray));
+// console.log(mergeSort(testArray));
 
 // 5. Sorting a linked list using merge sort
 const linked = new LinkedList();
 linked.insertFirst(15);
 linked.insertFirst(5);
-linked.insertFirst(10);
-linked.insertFirst(1);
 linked.insertFirst(20);
+linked.insertFirst(8);
 
-// function linkedMSort(arr){
-//   return mergeSort(arr);
-// }
+
+function linkedMSort(linkedList){
+  const tempArr = [];
+  let current = linkedList.head;
+  while (current !== null) {
+    tempArr.push(current.value);
+    current = current.next;
+  }
+  mergeSort(tempArr);
+  const newLinkedList = new LinkedList();
+  for (let i = 0; i < tempArr.length; i++) {
+    if (tempArr[i] === tempArr[0]) {
+      newLinkedList.insertFirst(tempArr[i]);
+      continue;
+    }
+    newLinkedList.insertLast(tempArr[i])
+  }
+  return newLinkedList;
+}
+
+console.log(linkedMSort(linked))
